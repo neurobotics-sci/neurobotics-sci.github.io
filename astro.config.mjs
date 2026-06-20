e0af4305cc8670d7e393b800a1604f2035115abb
Kenneth Renshaw <kenneth@n150-bubo.sbalf.local>
2026-06-19 22:40:26 -0500

    Initial launch: Code and Architecture

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://www.neuroboticssci.ai', // Website root domain and hostname pointed to GitHub Pages
+});
